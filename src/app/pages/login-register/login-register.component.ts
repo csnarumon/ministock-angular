70c81cc0c1ecc591307be98b6dd4880577a718ed
Narumon Saiprom <n.saiprom@gmail.com>
2020-04-12 17:11:58 +0700

    update final project

--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login-register',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+  //ตัวแปร / properties
+  message:string = "Hello Angular";
+
+  //ตัวแปร object
+  profile = {
+    "name":"Narumon",
+    "tel":"0622926144",
+    "gender":"Female"
+  }
+
+  //2 ways data binding
+  userData={
+    "email":"",
+    "password":""
+
+  }
+
+
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  submitLogin(){
+    //alert("Hey Angular");
+    if(this.userData.email == "n.saipom@gmail.com" && this.userData.password == "1234"){
+      alert("Login Success");
+    this.router.navigate(['backend']);
+    }else{
+      alert("Login Fail");
+    }
   }
 
 }
